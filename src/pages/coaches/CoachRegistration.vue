9e7b2ef58509e047aeb3a75c7c1a90afa5813079
Levente26 <madi.levente26@gmail.com>
2022-05-17 12:08:19 +0200

    feat: register as a coach

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>register</div>
+  <section>
+    <base-card>
+      <h2>Register as a Coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {};
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: { CoachForm },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.push('/coaches');
+    },
+  },
+};
 </script>
 
 <style scoped></style>
