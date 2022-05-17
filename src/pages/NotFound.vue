da8cb457531df3905dcf6f7b33df0e08674ce42a
Levente26 <madi.levente26@gmail.com>
2022-05-17 17:18:19 +0200

    feat: everything is ready :)

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,13 +1,15 @@
 <template>
-    <div></div>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page could not be found - maybe check out all our</p>
+      <router-link to="/coaches">coaches</router-link>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
