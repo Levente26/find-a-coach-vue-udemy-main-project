292b50b92722d9557b9484658c9d71e157ada187
Levente26 <madi.levente26@gmail.com>
2022-05-17 13:58:52 +0200

    feat: receiving requests & firebase realtime database

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,1 +1,10 @@
-export default {};
+export default {
+    contactCoach(context, payload) {
+        const newRequest = {
+            coachId: payload.coachId,
+            userEmail: payload.email,
+            message: payload.message,
+        };
+        context.commit('addRequest', newRequest);
+    },
+};
